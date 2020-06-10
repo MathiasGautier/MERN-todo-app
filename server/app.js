42fc3ab4582403a115957a184fb4f89d81aa2b2b
Mathias Gautier <mths.gautier@gmail.com>
2020-06-10 17:42:24 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+app.use(cookieParser());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser:true,  useUnifiedTopology: true}, ()=>{
+    console.log('successfully connected to database👀');
+});
+
+const userRouter = require('./routes/User');
+app.use('/user', userRouter);
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log('express server started 👌');
+});
---
+++ server/models/Todo.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:true
+    },
+    description:{
+        type: String,
+        required : true
+    },
+    image :{
+        type:String
+    }
+
+});
+
+
+module.exports = mongoose.model('Todo', TodoSchema);
