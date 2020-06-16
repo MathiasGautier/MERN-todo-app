171a793ed706ba560732a3a0084e9fecfb86cf5e
Mathias Gautier <mths.gautier@gmail.com>
2020-06-16 16:13:05 +0200

    code done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import Navbar from './Components/Navbar';
-import Login from './Components/Login';
-import Home from './Components/Home';
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
 import Register from "./Components/Register";
 import Todos from "./Components/Todos";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Admin from "./Components/Admin";
+import PrivateRoute from "./higher-order-components/PrivateRoute";
+import UnPrivateRoute from "./higher-order-components/UnPrivateRoute";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-     <div className="container">
-    <Router>
-        <Navbar/>
-        <Route exact path="/" component={Home}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/register" component={Register}/>
-        <Route path="/todos" component={Todos}/>
-    </Router>
-     </div>
+    <div className="container">
+      <Router>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <UnPrivateRoute path="/login" component={Login} />
+        <UnPrivateRoute path="/register" component={Register} />
+        <PrivateRoute path="/todos" roles={["user", "admin"]} component={Todos} />
+        <PrivateRoute path="/admin" roles={["admin"]} component={Admin} />
+      </Router>
+    </div>
   );
 }
 
